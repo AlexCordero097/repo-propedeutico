f91e6c8999123b5ee07785cfd6a163f936a67a83
Alex Cordero <victor_cordero97@hotmail.com>
2024-08-07 21:13:46 -0600

    Clase07082024

--- Clase07082024/01variablesnumericas.js
+++ Clase07082024/01variablesnumericas.js
@@ -12,8 +12,14 @@ var kilometros= metros/1000;
 console.log("En Kilometros es "+kilometros);
 */
 
+/*
 var peso= prompt("Introduce un peso");
 var altura= prompt("Introduce una altura");
 var imc= peso/altura*altura;
 
 console.log("El imc es " +imc);
+*/
+
+var celcius= prompt("Introduce los grados celcius");
+var f= (9/5) * celcius+32
+console.log("El resultado es" +f);
